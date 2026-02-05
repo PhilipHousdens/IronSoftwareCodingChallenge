6b85bdb735c6abbab325536ee103ee156a139a70
PhilipHousden <philip.housden.04@gmail.com>
2026-02-05 15:59:04 +0700

    [added]: the indexing of the index counting

--- OldPhonePadDecoder.cs
+++ OldPhonePadDecoder.cs
@@ -4,16 +4,30 @@ using System.Text;
 
 namespace IronSoftwareCodingChallenge
 {
-    private static readonly Dictionary<char, string> Keypad = new()
+    public static class OldPhonePadConverter
     {
-        {'2', "ABC"},
-        {'3', "DEF"},
-        {'4', "GHI"},
-        {'5', "JKL"},
-        {'6', "MNO"},
-        {'7', "PQRS"},
-        {'8', "TUV"},
-        {'9', "WXYZ"},
-        {'0', " "}
-    };
+        private static readonly Dictionary<char, string> Keypad = new()
+        {
+            { '1', "&'(" },
+            { '2', "ABC" },
+            { '3', "DEF" },
+            { '4', "GHI" },
+            { '5', "JKL" },
+            { '6', "MNO" },
+            { '7', "PQRS" },
+            { '8', "TUV" },
+            { '9', "WXYZ" },
+            { '0', " " }
+        };
+
+        private static void AppendChar(StringBuilder result, char key, int count)
+        {
+            if (count == 0 || !Keypad.ContainsKey(key))
+                return;
+
+            string letters = Keypad[key];
+            int index = (count - 1) % letters.Length;
+            result.Append(letters[index]);
+        }
+    }
 }
