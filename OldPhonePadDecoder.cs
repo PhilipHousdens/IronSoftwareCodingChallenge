0fa302dac41ae10344396f79d0715549ed98097a
PhilipHousden <philip.housden.04@gmail.com>
2026-02-05 15:50:06 +0700

    [add]: the Dictionary to map the char of input to the string inside

---
+++ OldPhonePadDecoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronSoftwareCodingChallenge
+{
+    private static readonly Dictionary<char, string> Keypad = new()
+    {
+        {'2', "ABC"},
+        {'3', "DEF"},
+        {'4', "GHI"},
+        {'5', "JKL"},
+        {'6', "MNO"},
+        {'7', "PQRS"},
+        {'8', "TUV"},
+        {'9', "WXYZ"},
+        {'0', " "}
+    };
+}
