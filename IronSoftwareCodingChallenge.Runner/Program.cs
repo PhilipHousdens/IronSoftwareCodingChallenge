58e59aa594ad3966f6834e49c8240e8b5ad3f2aa
PhilipHousden <philip.housden.04@gmail.com>
2026-02-07 18:48:40 +0700

    [Add]: runner project

---
+++ IronSoftwareCodingChallenge.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IronSoftwareCodingChallenge;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputs =
+        {
+            "33#",
+            "227*#",
+            "4433555 555666#",
+            "8 88777444666*664#" // correct out put is "TURING"
+        };
+
+        foreach (var input in inputs)
+        {
+            var output = OldPhonePadConverter.OldPhonePadDecoder(input);
+            Console.WriteLine($"Input: {input} => Output: {output}");
+        }
+    }
+}
